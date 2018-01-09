7b83c30145ebca43952b8b32f19dfdd3c4253ffa
suganthskce <suganth2610@gmail.com>
2018-01-09 19:41:45 +0530

    Create Program.c

---
+++ January Challenge 2018/Maximum Score/Program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    long int t;
+    scanf("%ld",&t);
+    while(t--) {
+        long int n;
+        scanf("%ld",&n);
+        long int arr[n][n],i,j,k,l,sum=0,max=0;
+        for(i=0;i<n;i++)
+            for(j=0;j<n;j++)
+                scanf("%ld",&arr[i][j]);
+        for(i=0;i<n;i++)
+            if(max<arr[n-1][i])
+                max = arr[n-1][i];
+        sum += max;
+        for(i=n-2;i>=0;i--) {
+            long int localMax = 0;
+            for(j=0;j<n;j++) {
+                if(arr[i][j]<max && localMax<arr[i][j])
+                    localMax = arr[i][j];
+            }
+            if(!localMax)
+                break;
+            sum += localMax;
+        }
+        if(i<0){
+            printf("%ld\n",sum);
+        } else {
+            printf("-1\n");
+        }
+    }   
+    return 0;
+}
